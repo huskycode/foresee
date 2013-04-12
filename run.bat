ec6ff3e245b249ec2ed5bdc8249d2887adc179e7
varokas <varokas@panusuwan.com>
2013-04-11 17:43:29 -0700

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,1 +1,25 @@
-node build/make_bootstrap.js %1
+@ECHO OFF
+
+WHERE node /Q
+IF ERRORLEVEL 1 (
+  ECHO node is required but is not installed.
+	GOTO :out
+)
+
+WHERE npm /Q
+IF ERRORLEVEL 1 (
+	ECHO npm is required but is not installed.
+	GOTO :out
+)
+
+WHERE coffee /Q
+IF ERRORLEVEL 1 (
+	call npm install coffee-script -g
+)
+
+IF NOT EXIST node_modules\coffee-script call npm install coffee-script
+IF NOT EXIST node_modules\shelljs call npm install shelljs
+
+coffee build\make.coffee %1
+
+:out

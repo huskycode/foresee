291a1fe537abe70b53843ca61976ee47728bbf24
Varokas Panusuwan <varokas@panusuwan.com>
2013-06-23 16:45:32 -0700

    Adds host page test

--- src/test/groovy/HomePageTest.groovy
+++ src/test/groovy/HomePageTest.groovy
@@ -4,27 +4,9 @@ import org.junit.*
 
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
+import page.HomePage
+import page.HostPage
 
-import static Info.*
-
-class Info {
-   static def FORESEE_BASE_URL = "http://localhost:3001/";
-}
-
-class HomePage extends Page {
-    static url = FORESEE_BASE_URL;
-    static at = { title == "Foresee" }
-    static content = {
-        roomNameTxt { $("#id") }
-        createRoomBtn { $("#createRoom") }
-    }
-}
-
-class HostPage extends Page {
-    static at = { title.startsWith("Host - ") }
-    static content = {
-    }
-}
 
 @RunWith(JUnit4)
 class HomePageTest extends GebReportingTest {
---
+++ src/test/groovy/page/Pages.groovy
@@ -0,0 +1,34 @@
+package page
+
+import geb.Page
+
+
+import static Info.*
+
+class Info {
+    static def FORESEE_BASE_URL = "http://localhost:3001/";
+}
+
+class HomePage extends Page {
+    static url = FORESEE_BASE_URL;
+    static at = { title == "Foresee" }
+    static content = {
+        roomNameTxt { $("#id") }
+        createRoomBtn { $("#createRoom") }
+    }
+}
+
+class HostPage extends Page {
+    static at = { title.startsWith("Host - ") }
+    static content = {
+        link { $("#link") }
+        storyDesc { $("input#storyDesc[type='text']") }
+        addStory { $("button#addStory") }
+        storyPile { $("ul#story-pile") }
+        qrCodeImg { $("div#qrcode>img") }
+        qrCode { $("div#qrcode") }
+        startNow { $("input#startNow[type='button']") }
+
+        findStoryPileOne(wait:true) { $("ul#story-pile>li") }
+    }
+}

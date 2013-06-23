2dad417593c2976ae0a80a1150a4cc3315d4ee51
Varokas Panusuwan <varokas@panusuwan.com>
2013-06-23 15:48:29 -0700

    Make test suite

---
+++ src/test/groovy/WebTestSuite.groovy
@@ -0,0 +1,42 @@
+import org.junit.AfterClass
+import org.junit.BeforeClass
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses([
+    HomePageTest.class
+])
+class WebTestSuite {
+    static def p
+
+    @BeforeClass
+    static void beforeClass() {
+        def pb = new ProcessBuilder("node","app").redirectErrorStream(true)
+        def env = pb.environment()
+        env.put("PORT", "3001")
+
+        p = pb.start()
+
+        def th1 = Thread.start {
+            BufferedReader stdoutReader = new BufferedReader(
+                    new InputStreamReader(p.getInputStream()));
+            String line;
+            try {
+                while ((line = stdoutReader.readLine()) != null) {
+                    println("[out] " + line)
+                }
+            } catch (IOException e) {
+            }
+        }
+    }
+
+    @AfterClass
+    static void afterClass() {
+        if(p != null) {
+            p.destroy()
+        }
+    }
+
+
+}

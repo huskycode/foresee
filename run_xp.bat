a400cf76a0e90c41d4316c7e61aa47d9abe0a1f6
Varokas Panusuwan <varokas@panusuwan.com>
2013-07-02 20:25:56 -0700

    simplify build script

--- run_xp.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-
-IF NOT EXIST node_modules\coffee-script call npm install coffee-script
-IF NOT EXIST node_modules\shelljs call npm install shelljs
-
-coffee build\make.coffee %1
-
-:out

ef3311d1707c8f84020b492055856d80348f163e
Varokas Panusuwan <varokas@panusuwan.com>
2013-07-02 20:12:07 -0700

    Re-enable commented out tests

--- src/test/groovy/WebTestSuite.groovy
+++ src/test/groovy/WebTestSuite.groovy
@@ -5,8 +5,8 @@ import org.junit.runners.Suite
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses([
-    //HomePageTest.class,
-    //HostPageTest.class,
+    HomePageTest.class,
+    HostPageTest.class,
     ClientTest.class
 ])
 class WebTestSuite {

029a1e4a9845224ed0ca168c1d135097f988b1d1
Varokas Panusuwan <varokas@panusuwan.com>
2013-07-18 08:41:09 -0700

    add test for on()

--- src/test/browsertest/services_test.js
+++ src/test/browsertest/services_test.js
@@ -1,23 +1,39 @@
-var mockSocketIo, webSocket;
+var mockSocketIo, mockSocket, webSocket, scope;
 
 describe("foresee.service.webSocket", function() {
 
     beforeEach(module('foresee'));
 
     beforeEach(function() {
-        mockSocketIo = { connect: jasmine.createSpy() };
+        mockSocketIo = { connect: null };
+        mockSocket = {
+            on: jasmine.createSpy(),
+            emit: jasmine.createSpy()
+        };
+
+        spyOn(mockSocketIo, 'connect').andReturn(mockSocket);
 
         module(function($provide) {
             $provide.value('socketIO', mockSocketIo);
         });
 
-        inject(function($injector) {
-            webSocket = $injector.get('webSocket');
+        inject(function($rootScope, webSocket) {
+            this.webSocket = webSocket;
         });
     });
 
     it('should call connect when the service is instantiated', function() {
         expect(mockSocketIo.connect).toHaveBeenCalled();
+    });
+
+    it('on() should call socketIO with correct event name', function() {
+        var callback = function(data) {
+
+        };
+
+        webSocket.on("myEvent", callback);
+
+        expect(mockSocket.on.mostRecentCall.args[0]).toEqual("myEvent");
     });
 
 });

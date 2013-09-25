c4a1e840d5ab20f218bc723cc423a16ac2d93fe6
Varokas Panusuwan <vpanusuw@cobaltgroup.com>
2013-09-25 12:27:39 -0700

    fix karma

--- karma.conf.js
+++ karma.conf.js
@@ -1,71 +1,26 @@
-// Karma configuration
-// Generated on Wed Jun 12 2013 01:58:20 GMT-0700 (PDT)
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine'],
+    files: [
+      'node_modules/chai/chai.js',
+      "src/test/browsertest/lib/jasmine/jasmine-html.js",
+      "src/test/browsertest/lib/jasmine/jasmine-ajax.js",
 
+      "assets/js/lib/angular/angular.min.js",
+      "assets/js/lib/underscore/underscore-min.js",
+      "assets/js/jquery-1.7.2.min.js",
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
-
-// list of files / patterns to load in the browser
-files = [
-  MOCHA,
-  MOCHA_ADAPTER,
-  'node_modules/chai/chai.js',
-  'node_modules/mocha/mocha.js',
-  'assets/js/jquery-1.7.2.min.js',
-  'assets/js/qrcode.js',
-  'assets/js/host.js',
-  'assets/js/index.js',
-  'test/browsertest/*_test.js'
-];
-
-
-// list of files to exclude
-exclude = [
-  
-];
-
-
-// test results reporter to use
-// possible values: 'dots', 'progress', 'junit'
-reporters = ['dots'];
-
-
-// web server port
-port = 9876;
-
-
-// cli runner port
-runnerPort = 9100;
-
-
-// enable / disable colors in the output (reporters and logs)
-colors = true;
-
-
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
-
-
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = true;
-
-
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Firefox'];
-
-
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 60000;
-
-
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = true;
+      "src/test/browsertest/lib/angular/angular-mocks.js",
+      'assets/js/jquery-1.7.2.min.js',
+      'assets/js/foresee.js',
+      'assets/js/index.js',
+      'assets/js/host2.js',
+      'assets/js/services.js',
+      'assets/js/participant.js',
+      'src/test/browsertest/*_test.js'
+    ],
+    browsers: ['Firefox'],
+    singleRun: true,
+  });
+};

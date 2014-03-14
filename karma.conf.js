477d502ebe17ec51eff38f8168bfce60553e91de
Varokas Panusuwan <varokas@panusuwan.com>
2014-03-13 19:16:50 -0700

    make karma works

--- karma.conf.js
+++ karma.conf.js
@@ -1,26 +1,40 @@
 module.exports = function(config) {
   config.set({
+
     basePath: '',
     frameworks: ['jasmine'],
     files: [
-      'node_modules/chai/chai.js',
       "src/test/browsertest/lib/jasmine/jasmine-html.js",
       "src/test/browsertest/lib/jasmine/jasmine-ajax.js",
 
-      "assets/js/lib/angular/angular.min.js",
-      "assets/js/lib/underscore/underscore-min.js",
-      "assets/js/jquery-1.7.2.min.js",
+      "frontend/js/lib/angular/angular.min.js",
+      "frontend/js/lib/angular/angular-route.min.js",
+      "frontend/js/lib/underscore/underscore-min.js",
 
       "src/test/browsertest/lib/angular/angular-mocks.js",
-      'assets/js/jquery-1.7.2.min.js',
-      'assets/js/foresee.js',
-      'assets/js/index.js',
-      'assets/js/host2.js',
-      'assets/js/services.js',
-      'assets/js/participant.js',
+      'frontend/js/lib/jquery/jquery-1.7.2.min.js',
+      'frontend/js/app.js',
+      'frontend/js/controllers.js',
+      'frontend/js/services.js',
+      'frontend/js/services.js',
       'src/test/browsertest/*_test.js'
     ],
-    browsers: ['Firefox'],
-    singleRun: true,
-  });
+
+    reporters: ['progress', 'junit'],
+    junitReporter: {
+      // will be resolved to basePath (in the same way as files/exclude patterns)
+      outputFile: 'test-results.xml'
+    },
+
+    colors: true,
+    autoWatch: true,
+    browsers: ["Chrome"],
+    singleRun: false,
+    plugins: [
+      'karma-jasmine',
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
+      'karma-junit-reporter'
+    ]
+    });
 };

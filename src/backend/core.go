0ed12baad67ad1729baad75a5d88f4adffada839
Varokas Panusuwan <varokas@panusuwan.com>
2014-04-27 13:43:21 -0700

    make create core returns pointer. fix Core interface

--- src/backend/core.go
+++ src/backend/core.go
@@ -2,27 +2,29 @@ package foresee_backend
 
 type Core interface {
   AddParticipant(room string, participant string)
-  ListParticipants(room string)
   RemoveParticipant(room string, participant string)
-  Vote(room string, participant string, vote int)
+  GetVotes(room string)
 }
 
 type CoreImpl struct {
   dataStore DataStore
 }
 
-func CreateCoreImpl(ds DataStore) CoreImpl {
-  return CoreImpl{ds}
+func CreateCoreImpl(ds DataStore) *CoreImpl {
+  coreImpl := new(CoreImpl)
+  coreImpl.dataStore = ds
+
+  return coreImpl
 }
 
-func (ci CoreImpl) AddParticipant(room string, participant string) {
+func (ci *CoreImpl) AddParticipant(room string, participant string) {
   ci.dataStore.Get(room).AddParticipant(participant)
 }
 
-func (ci CoreImpl) RemoveParticipant(room string, participant string) {
+func (ci *CoreImpl) RemoveParticipant(room string, participant string) {
   ci.dataStore.Get(room).RemoveParticipant(participant)
 }
 
-func (ci CoreImpl) GetVotes(room string) map[string]int {
+func (ci *CoreImpl) GetVotes(room string) map[string]int {
   return ci.dataStore.Get(room).GetVotes()
 }

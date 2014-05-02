56d563688b4534b052c4c74d927a53f8f0f4c0bb
Varokas Panusuwan <varokas@panusuwan.com>
2014-05-02 07:39:22 -0700

    add e2e test for home page blank

--- test/e2e/home_test.js
+++ test/e2e/home_test.js
@@ -1,6 +1,31 @@
+var HomePage = function() {
+  return {
+    URL: '/#/',
+
+    roomNameInput: element(by.model('roomName')),
+    createRoomButton: element(by.id('createRoom')),
+
+    get: function () { browser.get(this.URL); },
+
+    setRoomName: function (roomName) { this.roomNameInput.sendKeys(roomName); },
+    createRoom: function () { this.createRoomButton.click(); }
+  };
+};
+
+var HostPage = function(roomName) {
+  return {
+    URL: '/#/host/' + roomName,
+    get: function () { browser.get(this.URL); }
+
+
+  }
+};
+
 describe("Homepage Test", function() {
+    var homePage = HomePage()
+
     beforeEach(function() {
-        browser.get('/');
+        homePage.get();
     });
 
     it('should have correct title', function() {
@@ -8,14 +33,16 @@ describe("Homepage Test", function() {
     });
 
     it('should go to the host page when enter room name', function() {
-        element(by.model('roomName')).sendKeys('RoomABC');
-        element(by.id('createRoom')).click();
+        homePage.setRoomName("RoomABC");
+        homePage.createRoom();
 
-        expect( browser.getCurrentUrl()).toContain("/#/host/RoomABC");
+        expect( browser.getCurrentUrl()).toContain(HostPage("RoomABC").URL);
     });
 
     it('should stay at same page if no room name is entered', function() {
-        element(by.model('roomName')).sendKeys('');
-        element(by.id('createRoom')).click();
+        homePage.setRoomName("");
+        homePage.createRoom();
+
+        expect( browser.getCurrentUrl()).toMatch(homePage.URL + "$");
     });
 });
